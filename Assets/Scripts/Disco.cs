c20bdeb6e9142d7e9f33f715576490f0fbe988b6
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-01-04 15:42:45 -0700

    Some more work

---
+++ Assets/Scripts/Disco.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disco : MonoBehaviour {
+	[SerializeField]
+	Light discoLight;
+
+	// Update is called once per frame
+	void Update() {
+		discoLight.color = Random.ColorHSV();
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	[SerializeField]
+	[Range(-360, 360)]
+	float angle;
+
+	[SerializeField]
+	[Range(1, 5)]
+	float speed = 5.0f;
+
+	// Update is called once per frame
+	void Update() {
+		transform.rotation *= Quaternion.AngleAxis(angle * Time.deltaTime, Vector3.up);
+		if(Input.GetKey(KeyCode.LeftShift)) {
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+	}
+}
